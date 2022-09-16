ab0ecdc453d47c9c4dc255f47668466bbbb5f53c
cleo xia <cleoxia1@gmail.com>
2022-09-15 23:23:22 -0400

    add header

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: ______Wudi Xia______ Student ID: _108088154__ Date: __2022-09-15__
+*  Cyclic Link: __www.moviesapi.com________________________________________
+*
+********************************************************************************/ 
+
 const express = require("express");
 const app = express();
 app.use(express.json());
